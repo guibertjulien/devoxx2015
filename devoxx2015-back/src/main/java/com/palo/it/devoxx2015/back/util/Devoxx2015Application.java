05d244be0895194e080289fd6fcb1cfb08825472
alex-estela <aestela@palo-it.com>
2015-02-24 17:53:04 +0100

    Added Application impl

---
+++ devoxx2015-back/src/main/java/com/palo/it/devoxx2015/back/util/Devoxx2015Application.java
@@ -0,0 +1,8 @@
+package com.palo.it.devoxx2015.back.util;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class Devoxx2015Application extends Application {
+}
